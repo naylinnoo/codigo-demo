3f869a1418fb002e11117e0075155728d2bf2929
Nay Linn Oo <naylinnoo.1@gmail.com>
2021-06-12 20:13:21 +0630

    added footer

--- pages/footer.js
+++ pages/footer.js
@@ -4,13 +4,24 @@ import styles from '../styles/Footer.module.css'
 
 export default function Footer() {
     return (
-        <div className={' bg-white w-full'}>
-            <div className={'p-10 text-center'}>
+        <div className={' bg-white w-full my-10 '}>
+            <div className={'text-center'}>
                 <span className={styles.title}>Let's have a chat</span>
             </div>
-     Lets have a chat
-     asdf
-     asdfasdf
+            <div className={'grid 2xl:grid-cols-4 lg:grids-col-4 md:grid-cols-1 sm:grid-cols-1 grid-flow-row-dense pt-10 px-52'}>
+                <div className={'col-span-1'}>
+                    Build
+                </div>
+                <div className={'col-span-1'}>
+                    Co-incubate
+                </div>
+                <div className={'col-span-1'}>
+                    Customise
+                </div>
+                <div className={'col-span-1'}>
+                    Organise
+                </div>
+            </div>
         </div>
         
         )
